cbddb7d5f25550f98a4e68e43af5c257c8d186f5
henilagrawal <henilagrawal@gmail.com>
2026-07-15 22:14:54 -0700

    Init swift project

---
+++ ClosetScanner/ClosetScanner/ClosetScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetScannerApp.swift
+//  ClosetScanner
+//
+//  Created by Henil Agrawal on 2026-07-15.
+//
+
+import SwiftUI
+
+@main
+struct ClosetScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosetScanner/ClosetScannerTests/ClosetScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetScannerTests.swift
+//  ClosetScannerTests
+//
+//  Created by Henil Agrawal on 2026-07-15.
+//
+
+import Testing
+@testable import ClosetScanner
+
+struct ClosetScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
